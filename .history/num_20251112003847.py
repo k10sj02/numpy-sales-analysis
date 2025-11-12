d3eb636b3d179adf5fd5fae3381b2ed3f162930c
Stann-Omar Jones <35823259+k10sj02@users.noreply.github.com>
2025-11-12 01:27:54 -0500

    Remove editor history files

--- .history/num_20251112003847.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-months= np.array(['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'])
-sales= []
-print ("Enter the sales (in $1000) for each month")
