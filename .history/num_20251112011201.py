60e6b4132d8a913950f7990400cebff393f0cff7
Stann-Omar Jones <35823259+k10sj02@users.noreply.github.com>
2025-11-12 01:26:58 -0500

    remove .history

---
+++ .history/num_20251112011201.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+months= np.array(['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'])
+sales= []
+print ("Enter the sales (in $1000) for each month")
+
+for month in months:
+  value= float(input(f"{month}: "))
+  sales.append(value)
+
+sales= np.array(sales)
+print("\n ---- Company Sales Analysis---")
+print("Total Sales of the Year:", np.sum(sales), "$")
+print("Average Monthly Sales:", np.sum(sales), "$")
