7d7cbee15d162f1ce4ec310a4d5b3492ffbfb120
Stann-Omar Jones <35823259+k10sj02@users.noreply.github.com>
2025-11-12 01:09:35 -0500

    Initial commit for numpy-sales-analysis project

---
+++ .history/num_20251112003847.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+months= np.array(['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'])
+sales= []
+print ("Enter the sales (in $1000) for each month")
